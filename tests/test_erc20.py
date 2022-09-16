87341773a20d563851680265ff8462b016628915
Your Name <you@example.com>
2022-09-16 16:55:48 +0400

    add more test

--- tests/test_erc20.py
+++ tests/test_erc20.py
@@ -26,10 +26,21 @@ def test_token_minter_balance(erc20_contract, accounts):
     assert erc20_contract.balanceOf(accounts[0]) == _TOTAL_SUPPLY
     assert erc20_contract.totalSupply() == _TOTAL_SUPPLY
 
-def test_token_minter_balance(erc20_contract, accounts):
+def test_fail_minter_balance(erc20_contract, accounts):
     assert not erc20_contract.balanceOf(accounts[1]) == _TOTAL_SUPPLY
 
 def test_transfer(erc20_contract, accounts):
     erc20_contract.transfer(accounts[1], 1e18, {'from':accounts[0]})
     assert erc20_contract.balanceOf(accounts[1]) == 1e18
     assert erc20_contract.balanceOf(accounts[0]) == (1e24 - 1e18)
+
+def test_exception_transfer_from(erc20_contract, accounts):
+    with pytest.raises(Exception) :
+        res = erc20_contract.transferFrom(accounts[0], accounts[2], 2e18, {'from': accounts[3]})
+
+def test_allowance_and_transfer_from(erc20_contract, accounts):
+    res = erc20_contract.approve(accounts[3], 5e18, {'from': accounts[0]})
+    assert res
+    res = erc20_contract.transferFrom(accounts[0], accounts[2], 2e18, {'from': accounts[3]})
+    assert res
+    assert erc20_contract.balanceOf(accounts[2]) == 2e18
